e61a0ed52010593c7b27a1a91cb4ab8e8cef3a75
lolekmakso <makstarasovv1@gmail.com>
2024-03-12 22:21:56 +0200

    made swiper-cloth

--- js/swiper.js
+++ js/swiper.js
@@ -1,22 +1,47 @@
-let swiper = new Swiper(".mySwiper", {
-    effect: "fade", // Применяем эффект fade
-    fadeEffect: {
-        crossFade: true, // Включаем плавный переход между слайдами
-    },
-    spaceBetween: 30,
+// let swiper = new Swiper(".mySwiper", {
+//     effect: "fade", // Применяем эффект fade
+//     fadeEffect: {
+//         crossFade: true, // Включаем плавный переход между слайдами
+//     },
+//     spaceBetween: 30,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true, // Делаем пагинацию кликабельной
+//     },
+//     autoplay: {
+//         delay: 6000, // Изменяем задержку до 6 секунд
+//         disableOnInteraction: false,
+//     },
+//     mousewheel: false, // Отключаем скроллинг слайдера колесом мыши
+//     keyboard: false, // Отключаем управление слайдером с клавиатуры
+//     loop: true, // Включаем зацикливание слайдера
+// });
+
+const swiperOne = new Swiper('.cloth-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    speed: 800,
+  
+  
+    // Navigation arrows
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true, // Делаем пагинацию кликабельной
+
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+        1440: {
+            slidesPerView: 4,
+        }
     },
-    autoplay: {
-        delay: 6000, // Изменяем задержку до 6 секунд
-        disableOnInteraction: false,
-    },
-    mousewheel: false, // Отключаем скроллинг слайдера колесом мыши
-    keyboard: false, // Отключаем управление слайдером с клавиатуры
-    loop: true, // Включаем зацикливание слайдера
-});
+  });
