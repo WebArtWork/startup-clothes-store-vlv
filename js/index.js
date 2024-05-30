8bfe41b571fdaac3540d0e8bf5bbde958d901b52
nikiquickie <danzi6386@gmail.com>
2024-05-30 14:05:36 +0300

    fixed url (if token) 2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
-if (http.headers['token']){
+import Dom from "/api/wjst/dom";
+import Http from '/api/wjst/http';
+
+if (Http.headers['token']){
     Dom.element('icon_profile').href = '/profile';
 }
