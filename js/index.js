81af3e9cba0a29ca511b5ad00aa47e9c11ab4667
nikiquickie <danzi6386@gmail.com>
2024-06-27 13:35:56 +0300

    upadet design land page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
 function setMainPhoto(newSrc) {
     var mainPhoto = document.getElementById('mainPhoto');
     mainPhoto.src = newSrc;
-}  
+};  
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Перевірка розміру екрану
+    if (window.innerWidth > 768) { // Припустимо, що ви хочете активувати на екранах ширше 768px
+        let dropdownHoverItems = document.querySelectorAll('.dropdown-hover');
+
+        dropdownHoverItems.forEach(item => {
+            let button = item.querySelector('.button-menu');
+            let dropdownMenu = item.querySelector('.dropdown-menu');
+
+            button.addEventListener('mouseenter', function() {
+                dropdownMenu.classList.add('show');
+            });
+
+            item.addEventListener('mouseleave', function() {
+                dropdownMenu.classList.remove('show');
+            });
+        });
+    }
+});
