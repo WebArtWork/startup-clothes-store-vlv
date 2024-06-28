802b40735438645455386e4c49f3428d6f3fd519
nikiquickie <danzi6386@gmail.com>
2024-06-28 13:28:53 +0300

    fix button tags

--- js/index.js
+++ js/index.js
@@ -1,29 +1,23 @@
 function setMainPhoto(newSrc) {
     var mainPhoto = document.getElementById('mainPhoto');
     mainPhoto.src = newSrc;
-};  
+};
 
-document.addEventListener("DOMContentLoaded", function() {
-    if (window.innerWidth > 768) {
+document.addEventListener("DOMContentLoaded", function () {
+    // Перевірка розміру екрану
+    if (window.innerWidth > 768) { // Припустимо, що ви хочете активувати на екранах ширше 768px
         let dropdownHoverItems = document.querySelectorAll('.dropdown-hover');
 
         dropdownHoverItems.forEach(item => {
             let button = item.querySelector('.button-menu');
-            let dropdownMenu = item.querySelector('.dropdown-menu');
-
-            button.addEventListener('mouseenter', function() {
+            button.addEventListener('mouseenter', function () {
                 dropdownMenu.classList.add('show');
             });
+            let dropdownMenu = item.querySelector('.dropdown-menu');
 
-            item.addEventListener('mouseleave', function() {
+
+            item.addEventListener('mouseleave', function () {
                 dropdownMenu.classList.remove('show');
-            });
-
-            // Close dropdown when clicking outside
-            document.addEventListener('click', function(e) {
-                if (!item.contains(e.target)) {
-                    dropdownMenu.classList.remove('show');
-                }
             });
         });
     }
