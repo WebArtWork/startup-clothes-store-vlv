3e108e9b5c6af25f86ab9585c79bf1f22ac31934
nikiquickie <danzi6386@gmail.com>
2024-06-28 12:15:34 +0300

    fix tags menu

--- js/index.js
+++ js/index.js
@@ -3,22 +3,22 @@ function setMainPhoto(newSrc) {
     mainPhoto.src = newSrc;
 };  
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Перевірка розміру екрану
-    if (window.innerWidth > 768) { // Припустимо, що ви хочете активувати на екранах ширше 768px
-        let dropdownHoverItems = document.querySelectorAll('.dropdown-hover');
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Перевірка розміру екрану
+//     if (window.innerWidth > 768) { // Припустимо, що ви хочете активувати на екранах ширше 768px
+//         let dropdownHoverItems = document.querySelectorAll('.dropdown-hover');
 
-        dropdownHoverItems.forEach(item => {
-            let button = item.querySelector('.button-menu');
-            button.addEventListener('mouseenter', function() {
-                dropdownMenu.classList.add('show');
-            });
-            let dropdownMenu = item.querySelector('.dropdown-menu');
+//         dropdownHoverItems.forEach(item => {
+//             let button = item.querySelector('.button-menu');
+//             button.addEventListener('mouseenter', function() {
+//                 dropdownMenu.classList.add('show');
+//             });
+//             let dropdownMenu = item.querySelector('.dropdown-menu');
 
 
-            item.addEventListener('mouseleave', function() {
-                dropdownMenu.classList.remove('show');
-            });
-        });
-    }
-});
+//             item.addEventListener('mouseleave', function() {
+//                 dropdownMenu.classList.remove('show');
+//             });
+//         });
+//     }
+// });
