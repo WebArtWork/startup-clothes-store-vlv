930d13f292ba75413ab7ea9e07dbeaad43c1e027
nikiquickie <danzi6386@gmail.com>
2024-07-03 22:42:16 +0300

    update burger menu 13

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 item.addEventListener('click', function(e) {
                     console.log('test');
-                    dropdownMenu.classList.toggle('show');
+                    dropdownMenu.classList.add('show');
                 });
             }
         });
