0e5700a95a1da77e21b1ad51cbe53dfa67800095
nikiquickie <danzi6386@gmail.com>
2024-10-10 17:30:08 +0300

    update design swiper

--- js/swiper.js
+++ js/swiper.js
@@ -19,31 +19,37 @@ let swiper = new Swiper(".mySwiper", {
 const swiperCloth = new Swiper('.cloth-swiper', {
     slidesPerView: 1,
     spaceBetween: 20,
-    loop: true,
+    loop: document.querySelectorAll('.swiper-slide').length > 4, // Enable loop only if more than 4 slides
     speed: 800,
-  
-  
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
     breakpoints: {
         600: {
             slidesPerView: 2,
-            spaceBetween: 20
+            spaceBetween: 20,
         },
         992: {
             slidesPerView: 3,
-            spaceBetween: 20
+            spaceBetween: 20,
         },
         1350: {
             slidesPerView: 4,
-            spaceBetween: 20
-        }
+            spaceBetween: 20,
+        },
     },
     autoplay: {
         delay: 8000,
         disableOnInteraction: true,
-      },
-  });
+    },
+    on: {
+        init: function () {
+            const totalSlides = this.slides.length;
+            if (totalSlides <= 4) {
+                this.navigation.$nextEl.addClass('hidden');
+                this.navigation.$prevEl.addClass('hidden');
+            }
+        }
+    }
+});
